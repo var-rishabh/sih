8b1fde548951ebe0b086600f3fa1baaf835e955b
nazims-flow <nazimmazumder68@gmail.com>
2022-03-09 22:30:01 +0530

    data added

---
+++ data.js
@@ -0,0 +1,122 @@
+const data = [
+    {
+        firstName : "Rishabh",
+        middleName: "",
+        lastName:"Varshney",       
+        aadharNumber : 123456,
+        DOB : {
+            day:12,
+            month:03,
+            year:2002
+        },
+        age:20,
+        address:"aligarh",
+        SID:456709876842,
+
+    },
+    {
+        firstName : "Nazim",
+        middleName: "",
+        lastName:"Mazumder",       
+        aadharNumber : 456789,
+        DOB : {
+            day:12,
+            month:07,
+            year:2002
+        },
+        age:20,
+        address:"silchar",
+        SID:786709876842,
+
+    },
+    {
+        firstName : "Alex",
+        middleName: "",
+        lastName:"Sinha",       
+        aadharNumber : 987654,
+        DOB : {
+            day:12,
+            month:07,
+            year:2002
+        },
+        age:20,
+        address:"Delhi",
+        SID:786709876843,
+
+    },
+    {
+        firstName : "Rohit",
+        middleName: "",
+        lastName:"verma",       
+        aadharNumber : 987653,
+        DOB : {
+            day:13,
+            month:08,
+            year:2002
+        },
+        age:20,
+        address:"Delhi",
+        SID:786709876844,
+
+    },
+    {
+        firstName : "John",
+        middleName: "",
+        lastName:"Songate",       
+        aadharNumber : 987654,
+        DOB : {
+            day:13,
+            month:09,
+            year:2002
+        },
+        age:20,
+        address:"Silchar",
+        SID:786709876846,
+
+    },
+    {
+        firstName : "Vinit",
+        middleName: "",
+        lastName:"Singh",       
+        aadharNumber : 987655,
+        DOB : {
+            day:13,
+            month:09,
+            year:2002
+        },
+        age:20,
+        address:"Fatehpur",
+        SID:786709876845,
+
+    },
+    {
+        firstName : "Mudit",
+        middleName: "",
+        lastName:"Gupta",       
+        aadharNumber : 987656,
+        DOB : {
+            day:13,
+            month:10,
+            year:2002
+        },
+        age:20,
+        address:"Rohtak",
+        SID:786709876847,
+
+    },
+    {
+        firstName : "Syed",
+        middleName: "",
+        lastName:"Saquib",       
+        aadharNumber : 987657,
+        DOB : {
+            day:13,
+            month:11,
+            year:2002
+        },
+        age:20,
+        address:"Faridabad",
+        SID:786709876847,
+
+    }  
+  ];
