29e58042b1a1a068a27e2e51f0f7b6d4d147f431
nazims-flow <nazimmazumder68@gmail.com>
2022-03-10 18:51:29 +0530

    signIn session controller created

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,6 +1,21 @@
+const data = require('../data');
+
+
 exports.signIn =(req,res)=>{
     res.render("student");
 };
+
+
+exports.signInsession =  (req,res)=>{
+    console.log(req.body)
+
+    console.log(data[0].firstName);
+
+    res.status(200).json({
+        message:"all ok"
+    })
+}
+
 
 exports.signUp =(req,res)=>{
     res.render("student");
