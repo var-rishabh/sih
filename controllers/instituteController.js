5a35777a9fe93b308e72dc8783c0510155439027
var-rishabh <right.rishabh@gmail.com>
2022-03-13 00:54:15 +0530

    Institute Home Page completed

--- controllers/instituteController.js
+++ controllers/instituteController.js
@@ -6,26 +6,28 @@ exports.signIn = (req, res) => {
 
 exports.signInsession = (req,res)=>{
     let { institute_id, password } = req.body;
-    // let num = 0;
+    let num = 0;
     for (let d of data) {
         if (d.inst_Id === institute_id) {
             if (d.password === password) {
-                // res.render("institute_profile", { info : data[num]});
-                res.render("institute_profile");
+                // res.render("institute_profile");
+                res.render("institute_profile", { info : data[num]});
             }
         }
-        // num++;
+        num++;
     }
 }
 
 exports.signUp = (req,res)=>{
-    let { inst_id } = req.body;
-    // let num = 0;
+    let { inst_id, email } = req.body;
+    let num = 0;
     for (let d of data) {
         if (d.inst_Id == inst_id) {
-            // res.render("institute_profile", { info : data[num]});
-            res.render("institute_profile");
+            if (d.email === email) {
+                // res.render("institute_profile");
+                res.render("institute_profile", { info : data[num]});
+            }
         }
-        // num++;
+        num++;
     }
 }
