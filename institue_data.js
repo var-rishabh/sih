e29c1f032849934bc31ee45f242b13fd23d0bcb4
kumar9625 <rohitpawanverma9@gmail.com>
2022-03-12 19:22:50 +0530

    Create institue_data.js
    All Data Needed For Institute

---
+++ institue_data.js
@@ -0,0 +1,52 @@
+//For Signup
+
+const data = [
+    {
+        inst_Id: "2163505",         // then it sends otp to the official email_id of the college!
+        email: "piet@gmail.com",
+        inst_name: "Panipat Institute Of Engineering And Technology",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647091932/institute_details/piet_hlfaft.png",
+    },
+    {
+        inst_Id: "2163506",         // then it sends otp to the official email_id of the college!
+        email: "nitk@gmail.com",
+        inst_name: "National Institute Of Technology, Kurukshetra",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647092035/institute_details/nitk_w9c7dj.png",
+    },
+    {
+        inst_Id: "2163507",         // then it sends otp to the official email_id of the college!
+        email: "dtu@gmail.com",
+        inst_name: "Delhi Institue Of tTechnology",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647091941/institute_details/dtu_iq9i7w.jpg",
+    },
+    {
+        inst_Id: "2163508",         // then it sends otp to the official email_id of the college!
+        email: "iitk@gmail.com",
+        inst_name: "Indian Institue Of Technology, Kharagpur",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647091938/institute_details/IIT_Kharagpur_Logo.svg_f5sbbk.png",
+    },
+    {
+        inst_Id: "2163509",         // then it sends otp to the official email_id of the college!
+        email: "info@piet.co.in",
+        inst_name: "Kurukshetra University, Kurukshetraa",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647092190/institute_details/Kurukshetra_University_logo_n6jq6r.png",
+    },
+    {
+        inst_Id: "2163510",         // then it sends otp to the official email_id of the college!
+        email: "mit@gmail.com",
+        inst_name: "Manipal Institute Of Technology",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647093035/institute_details/manipal-university-logo_ixyhym.png",
+    },
+    {
+        inst_Id: "2163511",         // then it sends otp to the official email_id of the college!
+        email: "hansraj@gmail.com",
+        inst_name: "Hansraj College",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647091933/institute_details/hansraj_gkrmpn.jpg",
+    },
+    {
+        inst_Id: "2163512",         // then it sends otp to the official email_id of the college!
+        email: "iitd@gmail.com",
+        inst_name: "Indian Institue Of Technology, Delhi",
+        inst_logo: "https://res.cloudinary.com/ddllqpyuz/image/upload/v1647091933/institute_details/Indian_Institute_of_Technology_Delhi_Logo_favijc.svg",
+    },
+    ]
